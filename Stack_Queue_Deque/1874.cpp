ce3d2af311d416adcd95f4a6a52f084e19a6f6f8
repusjh <repusjh@gmail.com>
2019-09-21 16:37:07 +0900

    solved

--- Stack_Queue_Deque/1874.cpp
+++ Stack_Queue_Deque/1874.cpp
@@ -1,26 +1,62 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int n, temp, j = 1;
+int n, j = 1, errorFlag = 0, recurCount = 0;
 stack<int> st;
 vector<char> PushPop;
 
-int main(){
-    cin >> n;
-    st.push(0);
+bool isInStack(int);
 
-    for(int i = 0; i < n; i++){
-        cin >> temp;
-        if (st.top() < temp) {
-            while(st.size() <= temp){
-                PushPop.push_back('+');
-                st.push(j++);
-            }
-        }
-        if (st.top() == temp){
-            PushPop.push_back('-');
-            st.pop();
-        }
-        if (st.top())
-    }
+int main() {
+	cin >> n;
+	st.push(0);
+	for (int i = 0; i < n; i++) {
+		int num;
+		cin >> num;
+
+		if (num > st.top()) {
+			for (; j <= num; j++) {
+				st.push(j);
+				PushPop.push_back('+');
+			}
+		}
+
+		if (recurCount != 0 && !isInStack(num)) {
+			errorFlag = 1;
+			break;
+		}
+		recurCount++;
+
+		if (num < st.top()) {
+			if (!isInStack(num)) {
+				errorFlag = 1;
+				break;
+			}
+			else {
+				while (st.top() != num) {
+					st.pop();
+					PushPop.push_back('-');
+				}
+			}
+		}
+
+		st.pop();
+		PushPop.push_back('-');
+	}
+
+	if (errorFlag)
+		cout << "NO";
+	else
+		for (auto a : PushPop)
+			cout << a << '\n';
+}
+
+bool isInStack(int n) {
+	stack<int> tempStack = st;
+	while (!tempStack.empty()) {
+		if (tempStack.top() == n)
+			return true;
+		tempStack.pop();
+	}
+	return false;
 }
